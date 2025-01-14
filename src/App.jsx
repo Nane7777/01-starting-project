228b75b1fe57800c4b669dd2df60174c7a16cf51
Nicolas Gemine <gemine_nicolas@hotmail.com>
2025-01-14 17:25:01 +0100

    140125 17h

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
+import {CORE_CONCEPTS} from "./data.js";
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            {...CORE_CONCEPTS[0]}
+                        />
+                        <CoreConcept
+                            {...CORE_CONCEPTS[1]}
+                        />
+                        <CoreConcept
+                            {...CORE_CONCEPTS[2]}
+                        />
+                        <CoreConcept
+                            {...CORE_CONCEPTS[3]}
+                        />
+
+                    </ul>
+                </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton>Components</TabButton>
+                        <TabButton>JSX</TabButton>
+                        <TabButton>Props</TabButton>
+                        <TabButton>State</TabButton>
+                    </menu>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
